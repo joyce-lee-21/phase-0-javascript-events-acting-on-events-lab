e12d029ec9f498c9afd5c13ca69c112d6a1b0dd4
Joyce Lee <joycelee901@gmail.com>
2021-04-27 15:50:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+};
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers,10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 10}px`;
+    }
+};
